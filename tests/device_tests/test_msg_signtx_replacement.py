e7421472a45dcf2378097fe9830a78dcc6873a16
Andrew Kozlik <andrew.kozlik@gmail.com>
2020-09-16 17:47:23 +0200

    fix device test

--- tests/device_tests/test_msg_signtx_replacement.py
+++ tests/device_tests/test_msg_signtx_replacement.py
@@ -27,14 +27,11 @@ B = messages.ButtonRequestType
 TX_CACHE_TESTNET = TxCache("Testnet")
 TX_CACHE_MAINNET = TxCache("Bitcoin")
 
-TXHASH_c63e24 = bytes.fromhex(
-    "c63e24ed820c5851b60c54613fbc4bcb37df6cd49b4c96143e99580a472f79fb"
+TXHASH_50f6f1 = bytes.fromhex(
+    "50f6f1209ca92d7359564be803cb2c932cde7d370f7cee50fd1fad6790f6206d"
 )
-TXHASH_c6be22 = bytes.fromhex(
-    "c6be22d34946593bcad1d2b013e12f74159e69574ffea21581dad115572e031c"
-)
-TXHASH_58497a = bytes.fromhex(
-    "58497a7757224d1ff1941488d23087071103e5bf855f4c1c44e5c8d9d82ca46e"
+TXHASH_beafc7 = bytes.fromhex(
+    "beafc7cbd873d06dbee88a7002768ad5864228639db514c81cfb29f108bb1e7a"
 )
 
 
@@ -44,40 +41,30 @@ def test_p2pkh_fee_bump(client):
     # input 1: 0.0010 BTC
     # tx: 58497a7757224d1ff1941488d23087071103e5bf855f4c1c44e5c8d9d82ca46e
     # input 1: 0.0011 BTC
-    # Original tx accepted by network: c63e24ed820c5851b60c54613fbc4bcb37df6cd49b4c96143e99580a472f79fb
-    # The transaction was produced for different address_n, so the publick keys don't match, which is why the test fails :-(.
+    # Original tx accepted by network: 1869cdbb3a86ab8b71a3e4a0d11135926b18f62bc0ebeb8e8a56635135616f00
 
     inp1 = messages.TxInputType(
-        address_n=parse_path("44h/0h/0h/0/0"),
-        amount=100000,
-        prev_hash=TXHASH_c6be22,
-        prev_index=1,
-        orig_hash=TXHASH_c63e24,
+        address_n=parse_path("44h/0h/0h/0/4"),
+        amount=174998,
+        prev_hash=TXHASH_beafc7,
+        prev_index=0,
+        orig_hash=TXHASH_50f6f1,
         orig_index=0,
     )
 
-    inp2 = messages.TxInputType(
-        address_n=parse_path("44h/0h/0h/0/1"),
-        amount=110000,
-        prev_hash=TXHASH_58497a,
-        prev_index=1,
-        orig_hash=TXHASH_c63e24,
-        orig_index=1,
-    )
-
     out1 = messages.TxOutputType(
-        address="15Jvu3nZNP7u2ipw2533Q9VVgEu2Lu9F2B",
-        amount=100000,
+        address_n=parse_path("44h/0h/0h/1/2"),
+        amount=174998 - 50000 - 15000,  # Originally fee was 11300, now 15000.
         script_type=messages.OutputScriptType.PAYTOADDRESS,
-        orig_hash=TXHASH_c63e24,
+        orig_hash=TXHASH_50f6f1,
         orig_index=0,
     )
 
     out2 = messages.TxOutputType(
-        address_n=parse_path("44h/0h/0h/1/0"),
-        amount=210000 - 100000 - 20000,
+        address="1GA9u9TfCG7SWmKCveBumdA1TZpfom6ZdJ",
+        amount=50000,
         script_type=messages.OutputScriptType.PAYTOADDRESS,
-        orig_hash=TXHASH_c63e24,
+        orig_hash=TXHASH_50f6f1,
         orig_index=1,
     )
 
@@ -85,33 +72,21 @@ def test_p2pkh_fee_bump(client):
         client.set_expected_responses(
             [
                 request_input(0),
-                request_meta(TXHASH_c63e24),
-                request_input(1),
+                request_meta(TXHASH_50f6f1),
                 request_output(0),
-                messages.ButtonRequest(code=B.ConfirmOutput),
+                request_output(0, TXHASH_50f6f1),
                 request_output(1),
+                request_output(1, TXHASH_50f6f1),
                 messages.ButtonRequest(code=B.SignTx),
                 request_input(0),
-                request_meta(TXHASH_c6be22),
-                request_input(0, TXHASH_c6be22),
-                request_output(0, TXHASH_c6be22),
-                request_output(1, TXHASH_c6be22),
-                request_input(1),
-                request_meta(TXHASH_58497a),
-                request_input(0, TXHASH_58497a),
-                request_output(0, TXHASH_58497a),
-                request_output(1, TXHASH_58497a),
-                request_input(0, TXHASH_c63e24),
-                request_input(0, TXHASH_c63e24),
-                request_input(1, TXHASH_c63e24),
-                request_output(0, TXHASH_c63e24),
-                request_output(1, TXHASH_c63e24),
+                request_meta(TXHASH_beafc7),
+                request_input(0, TXHASH_beafc7),
+                request_output(0, TXHASH_beafc7),
+                request_input(0, TXHASH_50f6f1),
+                request_input(0, TXHASH_50f6f1),
+                request_output(0, TXHASH_50f6f1),
+                request_output(1, TXHASH_50f6f1),
                 request_input(0),
-                request_input(1),
-                request_output(0),
-                request_output(1),
-                request_input(0),
-                request_input(1),
                 request_output(0),
                 request_output(1),
                 request_output(0),
@@ -120,5 +95,5 @@ def test_p2pkh_fee_bump(client):
             ]
         )
         _, serialized_tx = btc.sign_tx(
-            client, "Bitcoin", [inp1, inp2], [out1, out2], prev_txes=TX_CACHE_MAINNET,
+            client, "Bitcoin", [inp1], [out1, out2], prev_txes=TX_CACHE_MAINNET,
         )
